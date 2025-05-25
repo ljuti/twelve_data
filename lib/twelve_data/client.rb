d9ea87d79d707bc62f5670b8129d2f186e2ba7ca
Lauri Jutila <git@laurijutila.com>
2025-05-25 19:29:06 +0300

    Fix retry middleware use.

--- lib/twelve_data/client.rb
+++ lib/twelve_data/client.rb
@@ -23,13 +23,13 @@ module TwelveData
     def connection
       @connection = Faraday.new(url: config.api_url) do |conn|
         conn.request :json
+        conn.request :retry, max: 3, interval: 0.05, backoff_factor: 2
         conn.response :json, content_type: /\bjson$/
         conn.adapter Faraday.default_adapter
         conn.headers["User-Agent"] = "TwelveData Ruby Gem #{TwelveData.gem_version}"
         conn.headers["Accept"] = "application/json"
         conn.headers["Content-Type"] = "application/json"
         conn.headers["Authorization"] = config.api_key
-        conn.use :retry, max: 3, interval: 0.05, backoff_factor: 2
       end
     end
   end
