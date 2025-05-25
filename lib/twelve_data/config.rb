58078cfffef9cd56962026fbba9cc1714be293ae
Lauri Jutila <git@laurijutila.com>
2025-05-25 19:30:41 +0300

    Fix Config defaults.

--- lib/twelve_data/config.rb
+++ lib/twelve_data/config.rb
@@ -3,7 +3,7 @@ require "anyway_config"
 module TwelveData
   class Config < Anyway::Config
     attr_config :api_key
-    attr_config :api_url, default: "https://api.twelvedata.com"
-    attr_config :ws_url, default: "wss://ws.twelvedata.com"
+    attr_config api_url: "https://api.twelvedata.com"
+    attr_config ws_url: "wss://ws.twelvedata.com"
   end
 end
